dae6573237907f0a27d7450d7a8c17c8071af09c
sora.watanabe <sorawsora02@gmail.com>
2020-06-01 02:44:36 +0000

    kadai-tasklist

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Task;
+
+class TasksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tasks=Task::All();
+        
+        return view('tasks.index',['tasks'=>$tasks],);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $task=new Task;
+        
+        return view('tasks.create',['task' => $task]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {   
+        $request->validate([
+            'content' => 'required|max:255',
+            'status' => 'required|max:10',
+            ]);
+        $task=new Task;
+        $task->content=$request->content;
+        $task->title=$request->status;
+        $task->save();
+        
+        return redirect('/');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $task=Task::findOrFail($id);
+        return view('tasks.show',['task' => $task,]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $task = Task::findOrFail($id);
+
+        // メッセージ編集ビューでそれを表示
+        return view('tasks.edit', [
+            'task' => $task,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'content'=>'required|max:255',
+             'status'=>'required|max:10',
+            ]);
+         $task = Task::findOrFail($id);
+        $task->content = $request->content;
+        $task->title=$request->status;
+        $task->save();
+
+        return redirect('/');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+         $task = Task::findOrFail($id);
+        $task->delete();
+
+        return redirect('/');
+    }
+}
---
+++ resources/views/tasks/edit.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+
+    @if(count($errors)>0)
+    <ul class="alert alert-danger" role="alert">
+        @foreach($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+    @endif
+     <h1>id: {{ $task->id }} のタスク編集ページ</h1>
+
+    <div class="row">
+        <div class="col-6">
+            {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+    <div class="form-group">
+                    {!! Form::label('status', 'status:') !!}
+                    {!! Form::text('title', null, ['class' => 'form-control']) !!}
+                </div>
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+
+                {!! Form::submit('更新', ['class' => 'btn btn-primary']) !!}
+
+            {!! Form::close() !!}
+        </div>
+    </div>
+@endsection
